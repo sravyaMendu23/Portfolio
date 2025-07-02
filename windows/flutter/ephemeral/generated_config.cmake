c1fec0c68b010d6db00f02674629816b82a46820
sravya-tc <sravya.mendu@toucanus.com>
2025-07-02 15:14:02 +0530

    Initial commit for portfolio

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.1.2+0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio"
+  "FLUTTER_TARGET=D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Backup_Documents\\SRAVYA-Portfolio\\sravya_portfolio\\.dart_tool\\package_config.json"
+)
